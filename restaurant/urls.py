11e442522fa7d8deb9f539ca0cf7ea8951d76529
Wisllem Barreto <wisllem@protonmail.com>
2025-05-06 20:14:56 -0300

    associating view menu_item with url and modifying menu.html with link to description

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('menu/', views.menu, name='menu'),
+    path('menu_item/<int:pk>/', views.display_menu_item, name='menu_item'),
 ]
