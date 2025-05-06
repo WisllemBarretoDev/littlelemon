6c4b50f085ed871f4c7e90d0cfbaae457026720a
Wisllem Barreto <wisllem@protonmail.com>
2025-05-06 19:22:32 -0300

    create view menu

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from . import models
 # Register your models here.
 
-admin.site.register(Menu)
+admin.site.register(models.Menu)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 def home(request):
     return HttpResponse('Home')
+
+def menu(request):
+    menu_data = models.Menu.objects.all()
+    return render('request', 'menu.html', {'menu' : menu_data} )
+
 
