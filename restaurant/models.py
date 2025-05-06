9dc9d681bd6b0e06d59a2e5ac4b81513b57fc520
Wisllem Barreto <wisllem@protonmail.com>
2025-05-06 19:56:50 -0300

    modified model, adding a new description attribute, done migrate

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
+    description = models.CharField(max_length=255, default='')
 
     def __str__(self):
         return self.name
