4f68f60f64f37b979ecee9df4d705165a351b81f
Wisllem Barreto <wisllem@protonmail.com>
2025-05-06 20:01:52 -0300

    additing view display_menu_item

--- restaurant/views.py
+++ restaurant/views.py
@@ -10,4 +10,12 @@ def menu(request):
     menu_data = models.Menu.objects.all()
     return render('request', 'restaurant/menu.html', {'menu' : menu_data} )
 
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = models.Menu.objects.get(pk=pk)
+    else:
+        menu_item = ''
+
+    return render(request, 'restaurant/menu_item.html', {'menu_item' : menu_item})
+
 
